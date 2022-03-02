9b11c92b9d9c5cc4fcc2e5d766149c0dcf6fdbeb
yonimal676 <malkayehonatan@gmail.com>
2022-03-02 17:00:10 +0200

    no comment

--- app/src/main/java/com/example/nahaganak/MainActivity.java
+++ app/src/main/java/com/example/nahaganak/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.nahaganak;
 
+
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.AppCompatDelegate;
 import android.os.Bundle;
+import java.util.ArrayList;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+public class MainActivity extends AppCompatActivity
+{
+
+    int LastLesson;
+    int TotalPayment;
+
+    ArrayList <Note> NotesArr = new ArrayList<>();
+
+
+    FloatingActionButton fab;
+
+
+
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+
+        // FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
